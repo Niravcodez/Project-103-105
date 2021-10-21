853aba9bcba686efb5f5bb5a7d3d82c7430c7218
Niravcodez <84302842+Niravcodez@users.noreply.github.com>
2021-10-20 19:11:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+Webcam.attach("#camera")
+
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id = 'captured_image' src='"+ data_uri +"'>";
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+Classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/yenbaDdMk//model.json", modelLoaded);
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+}
+
+function check()
+{
+    image = document.getElementById("captured_image");
+    Classifier.Classify(image, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("results_object_name").innerHTML = results[0].label;
+        document.getElementById("results_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
